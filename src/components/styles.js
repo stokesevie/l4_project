6e935b97f0a79ea267e539469376e0bee5b2edfd
stokesevie <2444030s@student.gla.ac.uk>
2022-10-27 18:55:01 +0100

    created email input

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,41 +1,95 @@
 import styled from 'styled-components/native';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 const statusBarHeight= Constants.statusBarHeight;
 
 export const colours = {
     primary: "#ffffff",
-    secondary: "#ADD8E6",
+    secondary: "#528AAE",
     thirdly: "#E1333F"
 };
 
 const {primary, secondary, thirdly} = colours;
 
-
-
-export const styledContainer = styled.View`
+export const StyledContainer = styled.View`
     flex: 1;
     padding: 25px;
     padding-top: ${statusBarHeight+10}px;
     background-color: ${primary};
 `;
 
-export const innerContainer = styled.View`
+export const InnerContainer = styled.View`
     flex: 1;
     width: 100%
     align-items: center;
 `;
-export const logo = styled.Image`
+export const Logo = styled.Image`
     width: 250px;
     height: 200px;
 `;
 
-export const pageTitle = styled.Text`
+export const PageTitle = styled.Text`
     font-size: 30px;
     text-align: center;
     font-weight: bold;
-    colour: ${secondary};
-    padding: 10
+    color: ${secondary};
+    padding: 10px;
 `;
 
+export const SubTitle= styled.Text`
+    font-size: 18px;
+    margin-bottom: 20px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    colour: black;
+`;
+
+export const StyledFormArea = styled.View`
+    width: 90%;
+`;
+export const StyledTextInput = styled.TextInput`
+    background-color: ${secondary};
+    padding: 15px;
+    padding-left: 55px;
+    padding-right: 55px;
+    border-radius: 5px;
+    font-size: 16px;
+    height: 60px;
+    margin-vertical: 3px;
+    margin-bottom: 10px;
+    color: black;
+`;
+export const StyledInputLabel = styled.Text`
+    color: black;
+    font-size: 15px;
+    text-align: left;
+`;
+
+
+export const LeftIcon = styled.View`
+    left: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+export const RightIcon = styled.TouchableOpacity`
+    left: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+    padding: 15px;
+    background-color: ${secondary};
+    justify-content: center;
+    border-radius: 5px;
+    margin-vertical: 5px;
+    height: 60px;
+`;
+
+export const ButtonText = styled.Text`
+    color: ${primary};
+    font-size: 16px;
+`;
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,20 +1,68 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
+import {Formik} from 'formik';
+import {Octicons} from '@expo/vector-icons';
 import {
-  styledContainer,
-  innerContainer,
-  logo,
-  pageTitle
+  StyledContainer,
+  InnerContainer,
+  Logo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  colours
 } from './../components/styles';
+
+import {View} from 'react-native'
+const {secondary,primary}= colours;
 
 const Login = () => {
   return (
-    <styledContainer>
-      <innerContainer>
-        <logo resizeMode="cover" source={require('./../assets/icon.png')} />
-        <pageTitle>Mindscape</pageTitle>
-      </innerContainer>
-    </styledContainer>
+    <StyledContainer>
+      <InnerContainer>
+        <StatusBar style="dark"/>
+        <Logo resizeMode="cover" source={require('./../assets/icon.png')} />
+        <PageTitle>Mindscape</PageTitle>
+        <SubTitle>Log in</SubTitle>
+        <Formik
+          initialValues={{email:'', password:''}}
+          onSubmit={(values)=> {
+            console.log(values)
+          }}
+        >
+          {({handleChange,handleBlur,handleSubmit,values})=> (<StyledFormArea>
+            <TextInput 
+              label = "Email"
+              icon ="mail"
+              placeholder = "john@mail.com"
+              placeholderTextColor={primary}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              keyboardType="email-address"
+            />
+          </StyledFormArea>)}
+
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
   );
+};
+
+const TextInput = ({label, icon, ...props}) =>{
+  return(
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={primary}/>
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props}/>
+    </View>
+  )
 }
+
 export default Login;
